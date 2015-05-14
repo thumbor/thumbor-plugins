b514e5ce0c1bc01de3067d9474440b803ca4622a
Marcin Kurzyna <marcin.kurzyna@nextwebventures.com>
2015-05-14 15:53:35 +0200

    Add Optipng optimizer
    - http://optipng.sourceforge.net/

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -18,6 +18,7 @@ from thumbor.context import Context
 __dirname = abspath(dirname(__file__))
 
 from thumbor_plugins.optimizers.pngcrush import Optimizer as PngcrushOptimizer
+from thumbor_plugins.optimizers.optipng import Optimizer as OptipngOptimizer
 
 fixtures_folder = join(abspath(dirname(__file__)), 'fixtures')
 
@@ -51,3 +52,32 @@ class PngcrushOptimizerTest(unittest.TestCase):
         self.assertLessEqual(os.path.getsize(temp.name), os.path.getsize(fixtures_folder + '/img/fixture2.png'),
                              "pngcrush could not lower filesize for img/fixture1.png")
 
+
+class OptipngOptimizerTest(unittest.TestCase):
+    def setUp(self):
+        self.optipng_path = '/usr/bin/optipng'
+        if not (os.path.isfile(self.optipng_path) and os.access(self.optipng_path, os.X_OK)):
+            raise unittest.SkipTest("Unable to locate optipng at {}".format(self.optipng_path))
+
+    def get_context(self):
+        conf = Config()
+        conf.OPTIPNG_PATH = self.optipng_path
+        conf.STATSD_HOST = ''
+
+        return Context(config=conf)
+
+    def test_optipng_should_not_run_for_jpeg(self):
+        optimizer = OptipngOptimizer(self.get_context())
+        self.assertFalse(optimizer.should_run('jpeg', None))
+
+    def test_optipng_should_run_for_png(self):
+        optimizer = OptipngOptimizer(self.get_context())
+        self.assertTrue(optimizer.should_run('png', None))
+
+    def test_optipng_should_optimize_png(self):
+        optimizer = OptipngOptimizer(self.get_context())
+        temp = tempfile.NamedTemporaryFile()
+        optimizer.optimize(None, fixtures_folder + '/img/fixture2.png', temp.name)
+
+        self.assertLessEqual(os.path.getsize(temp.name), os.path.getsize(fixtures_folder + '/img/fixture2.png'),
+                             "optipng could not lower filesize for img/fixture1.png")
