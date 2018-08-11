1cc69eced0bf09481cfa24a74fda2be10329417d
Jyrki Hokkanen <jyrki.hokkanen@sanoma.com>
2018-08-11 09:41:45 +0300

    cleanup temp file

--- thumbor_plugins/optimizers/mozjpeg.py
+++ thumbor_plugins/optimizers/mozjpeg.py
@@ -43,3 +43,4 @@ class Optimizer(BaseOptimizer):
         with open(os.devnull) as null:
             logger.debug("[MOZJPEG] running: " + command)
             subprocess.call(command, shell=True, stdin=null)
+            os.unlink(intermediary)
