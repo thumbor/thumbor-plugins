4bea1e232b059e68c6eb22b31c914aea88145699
Guilherme Souza <101073+guilhermef@users.noreply.github.com>
2022-01-31 00:56:25 +0100

    Add setup.py to mozlpeg

---
+++ thumbor_plugins/optimizers/mozjpeg/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='thumbor-plugins-mozjpeg',
+    version='0.0.1',
+    keywords='thumbor optimizers mozjpeg',
+    author='Guilherme Souza',
+    author_email='guilherme@souza.tech',
+    url='https://thumbor.readthedocs.io/en/latest/index.html',
+    license='MIT',
+    description='Thumbor optimizer to run mozjpeg',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Multimedia :: Graphics :: Presentation'
+    ],
+    packages=[
+        'thumbor_plugins.optimizers.mozjpeg',
+        'thumbor_plugins',
+    ],
+    package_dir= {
+        'thumbor_plugins': '../../..',
+        'thumbor_plugins.optimizers.mozjpeg': '.',
+    },
+)
