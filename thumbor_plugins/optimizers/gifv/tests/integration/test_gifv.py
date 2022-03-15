a61332a864e73debdb94f1527a8f4d17d26e947d
Guilherme Souza <101073+guilhermef@users.noreply.github.com>
2022-03-15 23:41:50 +0100

    fix(gifv): Add WebM tests

--- thumbor_plugins/optimizers/gifv/tests/integration/test_gifv.py
+++ thumbor_plugins/optimizers/gifv/tests/integration/test_gifv.py
@@ -11,6 +11,24 @@ class GIFVTest(EngineCase):
 
     @gen_test
     async def test_gifv(self):
-        await self.http_client.fetch(
+        response = await self.http_client.fetch(
             self.get_url("/unsafe/filters:gifv()/animated.gif")
         )
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.headers["Content-Type"], "video/mp4")
+
+    @gen_test
+    async def test_gifv_with_background(self):
+        response = await self.http_client.fetch(
+            self.get_url("/unsafe/filters:gifv(mp4):background_color(ff00ff)/animated.gif")
+        )
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.headers["Content-Type"], "video/mp4")
+
+    @gen_test
+    async def test_gifv_webm(self):
+        response = await self.http_client.fetch(
+            self.get_url("/unsafe/filters:gifv(webm)/animated.gif")
+        )
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.headers["Content-Type"], "video/webm")
